3d55da46bf881b887a63a46d3500cbfe8d47fd4f
vijendrabhandarkar <52195156+vijendrabhandarkar@users.noreply.github.com>
2023-02-02 16:14:05 +0530

    Project module

---
+++ ActiTIME/src/com/actitime/testscript/ProjectModule.java
@@ -0,0 +1,12 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ProjectModule {
+	@Test
+	public void testCreateProject()
+	{
+		Reporter.log("CreateProject",true);
+	}
+}
